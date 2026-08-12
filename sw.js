3ca22c98ad26ecd7ed74191e8ef6d615816a7398
Agustinadearagon <72619859+Agustinadearagon@users.noreply.github.com>
2026-08-12 17:57:56 +0200

    Update cache name to juegodepeones-v3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_NAME = "juegodepeones-v2";  // Cambia el número cada vez que hagas cambios importantes
+const CACHE_NAME = "juegodepeones-v3";  // Cambia el número cada vez que hagas cambios importantes
 
 const archivos = [
     "./",
